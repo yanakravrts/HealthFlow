7cbec857bbd80bd05c7c7da6b52118c115e27e9b
vladyslavkryskiv <114565602+vladyslavkryskiv@users.noreply.github.com>
2023-11-30 16:43:27 +0200

    Email

--- send_email/mailer.py
+++ send_email/mailer.py
@@ -6,16 +6,12 @@ from email.mime.text import MIMEText
 import random
 
 app = FastAPI()
-
-
 def generate_verification_code():
     return str(random.randint(100000, 999999))
-
 class EmailSchema(BaseModel):
     receiver_email: EmailStr
     subject: str
     body: str
-
 @app.post("/send_email/")
 async def send_email(email: EmailSchema):
     smtp_server = smtplib.SMTP("smtp.gmail.com", 587)
@@ -29,22 +25,13 @@ async def send_email(email: EmailSchema):
 
     try:
         smtp_server.login(sender_email, sender_password)
-
-
         verification_code = generate_verification_code()
-
-
-
-
         message = MIMEMultipart()
         message['From'] = sender_email
         message['To'] = receiver_email
         message['Subject'] = subject
-
         body_with_code = f"Verification Code: {verification_code}"
         message.attach(MIMEText(body_with_code, 'plain'))
-
-
         smtp_server.sendmail(sender_email, receiver_email, message.as_string())
 
         return {"message": "Email sent successfully"}
@@ -52,5 +39,3 @@ async def send_email(email: EmailSchema):
         raise HTTPException(status_code=500, detail=f"An error occurred: {str(e)}")
     finally:
         smtp_server.quit()
-
-
