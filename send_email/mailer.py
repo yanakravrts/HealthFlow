8251d9c5e83a192155726bac653391129382b1ca
vladyslavkryskiv <114565602+vladyslavkryskiv@users.noreply.github.com>
2023-12-01 18:46:48 +0200

    Email

--- send_email/mailer.py
+++ send_email/mailer.py
@@ -6,12 +6,20 @@ from email.mime.text import MIMEText
 import random
 
 app = FastAPI()
+
+verification_codes = {}  # Store verification codes and associated email addresses
+
+
 def generate_verification_code():
     return str(random.randint(100000, 999999))
+
+
 class EmailSchema(BaseModel):
     receiver_email: EmailStr
     subject: str
     body: str
+
+
 @app.post("/send_email/")
 async def send_email(email: EmailSchema):
     smtp_server = smtplib.SMTP("smtp.gmail.com", 587)
@@ -25,13 +33,18 @@ async def send_email(email: EmailSchema):
 
     try:
         smtp_server.login(sender_email, sender_password)
+
         verification_code = generate_verification_code()
+        verification_codes[receiver_email] = verification_code  # Store the code with email
+
         message = MIMEMultipart()
         message['From'] = sender_email
         message['To'] = receiver_email
         message['Subject'] = subject
+
         body_with_code = f"Verification Code: {verification_code}"
         message.attach(MIMEText(body_with_code, 'plain'))
+
         smtp_server.sendmail(sender_email, receiver_email, message.as_string())
 
         return {"message": "Email sent successfully"}
@@ -39,3 +52,12 @@ async def send_email(email: EmailSchema):
         raise HTTPException(status_code=500, detail=f"An error occurred: {str(e)}")
     finally:
         smtp_server.quit()
+
+
+@app.post("/check_email/")
+async def check_email(verification_code: str, receiver_email: EmailStr):
+    stored_code = verification_codes.get(receiver_email)
+    if stored_code and stored_code == verification_code:
+        return {"message": "Verification successful"}
+    else:
+        raise HTTPException(status_code=400, detail="Invalid verification code or email")
