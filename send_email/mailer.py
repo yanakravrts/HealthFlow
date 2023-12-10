55a801ed563b71f136edce842198ba2351a85535
Volodimir Obarianyk <volodimirobaranik@gmail.com>
2023-12-10 14:07:57 +0200

    Revert "Email"
    This reverts commit 0edf21abb4544d13e591a40c6a2d4a185ec51aa8.

--- send_email/mailer.py
+++ send_email/mailer.py
@@ -6,55 +6,56 @@ from email.mime.text import MIMEText
 import random
 from cachetools import TTLCache
 
-app = FastAPI()
+class EmailService:
+    def init(self):
+        self.smtp_server = smtplib.SMTP("smtp.gmail.com", 587)
+        self.smtp_server.starttls()
 
+        self.sender_email = 'spanchak228@gmail.com'
+        self.sender_password = 'zwne hugo slfv dacq'
+        self.verification_codes = TTLCache(maxsize=100, ttl=300)
 
-verification_codes = TTLCache(maxsize=100, ttl=300)
+    def generate_verification_code(self):
+        return str(random.randint(100000, 999999))
 
-def generate_verification_code():
-    return str(random.randint(100000, 999999))
+    def send_email(self, receiver_email, subject, body):
+        try:
+            self.smtp_server.login(self.sender_email, self.sender_password)
+
+            verification_code = self.generate_verification_code()
+            self.verification_codes[receiver_email] = verification_code
+
+            message = MIMEMultipart()
+            message['From'] = self.sender_email
+            message['To'] = receiver_email
+            message['Subject'] = subject
+
+            body_with_code = f"Verification Code: {verification_code}"
+            message.attach(MIMEText(body_with_code, 'plain'))
+
+            self.smtp_server.sendmail(self.sender_email, receiver_email, message.as_string())
+
+            return {"message": "Email sent successfully"}
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=f"An error occurred: {str(e)}")
+        finally:
+            self.smtp_server.quit()
 
 class EmailSchema(BaseModel):
     receiver_email: EmailStr
     subject: str
     body: str
 
+app = FastAPI()
+email_service = EmailService()
+
 @app.post("/send_email/")
 async def send_email(email: EmailSchema):
-    smtp_server = smtplib.SMTP("smtp.gmail.com", 587)
-    smtp_server.starttls()
-
-    sender_email = 'spanchak228@gmail.com'
-    sender_password = 'zwne hugo slfv dacq'
-    receiver_email = email.receiver_email
-    subject = email.subject
-    body = email.body
-
-    try:
-        smtp_server.login(sender_email, sender_password)
-
-        verification_code = generate_verification_code()
-        verification_codes[receiver_email] = verification_code  # Store the code with email
-
-        message = MIMEMultipart()
-        message['From'] = sender_email
-        message['To'] = receiver_email
-        message['Subject'] = subject
-
-        body_with_code = f"Verification Code: {verification_code}"
-        message.attach(MIMEText(body_with_code, 'plain'))
-
-        smtp_server.sendmail(sender_email, receiver_email, message.as_string())
-
-        return {"message": "Email sent successfully"}
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"An error occurred: {str(e)}")
-    finally:
-        smtp_server.quit()
+    return email_service.send_email(email.receiver_email, email.subject, email.body)
 
 @app.post("/check_email/")
 async def check_email(verification_code: str, receiver_email: EmailStr):
-    stored_code = verification_codes.get(receiver_email)
+    stored_code = email_service.verification_codes.get(receiver_email)
     if stored_code and stored_code == verification_code:
         return {"message": "Verification successful"}
     else:
